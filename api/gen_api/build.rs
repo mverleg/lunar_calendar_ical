ea4fa187be7156ed00edd34d747d33a970b4defb
Mark <mverleg.noreply@gmail.com>
2019-11-10 04:21:43 +0100

    Conversion from yaml to json done

--- api/gen_api/build.rs
+++ api/gen_api/build.rs
@@ -1,25 +1,40 @@
-use std::fs;
-use std::path::Path;
+use std::{env, fs};
 use std::path::PathBuf;
+
+use ::serde_yaml;
 
 fn main() {
 
-    // Get the schema directory.
+    // Get the schema and output directory.
     let schema_dir = fs::canonicalize(&PathBuf::from("../schema")).unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     assert!(schema_dir.is_dir());
+    assert!(out_dir.is_dir());
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed={}", schema_dir.to_string_lossy());
 
     let files = vec!["choose_date.yaml"];
     for filename in files {
-        let file = schema_dir.join(filename);
-        println!("cargo:rerun-if-changed={}", file.to_string_lossy());
 
-        
+        // Create path and make sure it causes cache invalidation on change.
+        let yaml_pth = schema_dir.join(filename);
+        println!("cargo:rerun-if-changed={}", yaml_pth.to_string_lossy());
+
+        // Read and parse the yaml.
+        let yaml_str = fs::read_to_string(&yaml_pth).unwrap();
+        let yaml_code: serde_json::Value = serde_yaml::from_str(&yaml_str).unwrap();
+
+        // Write to json.
+        let json_pth = {
+            let mut pth = PathBuf::from(yaml_pth.file_name().unwrap());
+            pth = out_dir.join(pth);
+            pth.set_extension("json");
+            pth
+        };
+        let json_str = serde_json::to_string(&yaml_code).unwrap();
+        fs::write(json_pth, json_str).unwrap();
     }
 
-    //TODO @mark: read yaml
-    //TODO @mark: write json
     //TODO @mark: json -> objects
 }
