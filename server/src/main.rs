4d9d922c546df318af15863433666ada5a9ab797
Mark <mverleg.noreply@gmail.com>
2019-11-10 01:55:32 +0100

    Separate projects for generated code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("Hello, world!");
     //TODO @mark: https://rust-lang.github.io/async-book/01_getting_started/05_http_server_example.html
+    //TODO @mark: something for form validation?
 }
