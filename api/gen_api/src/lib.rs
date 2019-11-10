0c7b37677fac7df6dc8613e771acbff932c853c3
Mark <mverleg.noreply@gmail.com>
2019-11-10 18:57:37 +0100

    Add a method to do messagepack serialization

--- api/gen_api/src/lib.rs
+++ api/gen_api/src/lib.rs
@@ -1,16 +1,29 @@
 #![feature(rustc_private)]
 
+use ::rmp_serde;
+
 include!(concat!(env!("OUT_DIR"), "/schema.rs"));
+
+trait ToMsgPack {
+    fn as_msg_pack(&mut self) -> Result<Vec<u8>, rmp_serde::encode::Error>;
+}
+
+impl <T> ToMsgPack for T where T: Serialize {
+    fn as_msg_pack(&mut self) -> Result<Vec<u8>, rmp_serde::encode::Error> {
+        let mut buf = Vec::new();
+        self.serialize(&mut rmp_serde::Serializer::new(&mut buf))?;
+        Ok(buf)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::Api;
-//    use super::Deserialize;
-//    use super::_IMPL_SERIALIZE_FOR_Api;
+    use super::ToMsgPack;
 
     #[test]
     fn generated_code_exists() {
-        let api: Api = serde_json::from_str(r#"{
+        let mut api: Api = serde_json::from_str(r#"{
             "initial_date": {
                 "calendar": "LunarChina",
                 "format": "yyyy-MM-dd",
@@ -20,5 +33,9 @@ mod tests {
             "repeat_mode": "yearly",
             "include_repetition_count": false
         }"#).unwrap();
+        let msg_pack = api.as_msg_pack().unwrap();
+        println!("{:?}", msg_pack);  //TODO @mark: TEMPORARY! REMOVE THIS!
+        println!("{:?}", msg_pack.len());  //TODO @mark: TEMPORARY! REMOVE THIS!
+        panic!();  //TODO @mark: TEMPORARY! REMOVE THIS!
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     println!("Hello, world!");
     //TODO @mark: https://rust-lang.github.io/async-book/01_getting_started/05_http_server_example.html
